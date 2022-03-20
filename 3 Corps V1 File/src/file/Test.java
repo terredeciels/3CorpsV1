b943bca2de1b9e242b4ef4ba959bba1939efc614
terredeciels <terredeciels>
2022-03-20 18:14:25 +0100

    fichier texte writeFile

--- 3 Corps V1 File/src/file/Test.java
+++ 3 Corps V1 File/src/file/Test.java
@@ -1,25 +1,13 @@
 package file;
 
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 
 public class Test {
 
     public static void main(String[] args) throws IOException {
-        String filename = "3corpsXYZ.txt";
-        String pathname = "C:\\Users\\gille\\IdeaProjects\\3 Corps V1\\";
-        File toFile = new File(pathname+filename);
-
-        if (toFile.createNewFile()) System.out.println("Fichier: " + toFile.getName());
-        else System.out.println("Le fichier existe");
-
-        FileWriter myWriter = new FileWriter(filename);
-        myWriter.write("Files in Java might be tricky, but it is fun enough!");
-
-        myWriter.close();
-
-        System.out.println("Succes");
+        final String filename = "3corpsXYZ.txt";
+        final String pathname = "C:\\Users\\gille\\IdeaProjects\\3 Corps V1\\";
+        new WriteToFile(pathname,filename);
 
     }
 }
--- 3 Corps V1 File/src/file/WriteToFile.java
+++ 3 Corps V1 File/src/file/WriteToFile.java
@@ -1,18 +1,23 @@
 package file;
 
-import java.io.FileWriter;   // Import the FileWriter class
-import java.io.IOException;  // Import the IOException class to handle errors
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class WriteToFile {
-    public static void main(String[] args) {
-        try {
-            FileWriter myWriter = new FileWriter("filename.txt");
-            myWriter.write("Files in Java might be tricky, but it is fun enough!");
-            myWriter.close();
-            System.out.println("Successfully wrote to the file.");
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
+
+    public WriteToFile(String pathname, String filename) throws IOException {
+        File toFile = new File(pathname+filename);
+
+        if (toFile.createNewFile()) System.out.println("Fichier: " + toFile.getName());
+        else System.out.println("Le fichier existe");
+
+        FileWriter writter = new FileWriter(filename);
+        writter.write("une ligne");
+
+        writter.close();
+
+        System.out.println("Succes");
     }
+
 }
